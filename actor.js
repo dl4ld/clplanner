349a8d885788d2bbbf753b94addbc8f43cbf45c8
Reggie <cushingreggie@gmail.com>
2021-02-03 16:02:59 +0100

    create planner module

---
+++ actor.js
@@ -0,0 +1,37 @@
+const cmdArgs = require('command-line-args')
+const fs = require('fs')
+const YAML = require('yaml')
+const parserEventNames = require("./parsers/event_names").parser;
+const parserEventExpr = require("./parsers/event_expr").parser;
+const parserSymbols = require("./parsers/symbols").parser;
+const parserExpression = require("./parsers/expression").parser;
+const planner = require('./planner/planner')
+const secureAmqp = require('../cllibsecureamqp')
+//const secureAmqp = require('secureamqp')
+
+const cmdOptions = [
+	{ name: 'send', alias: 's', type: String},
+	{ name: 'config', alias: 'c', type: String},
+	{ name: 'plan', alias: 'p', type: String}
+]
+
+const options = cmdArgs(cmdOptions)
+options.config = options.config || "./config"
+const config = require(options.config)
+const toAddress = options.send
+
+
+async function main() {
+	await secureAmqp.init(config)
+	const myAddress = secureAmqp.getMyAddress()
+	console.log("Actor address: ", myAddress)
+
+	secureAmqp.registerFunction('.f.handleResult', null, function(req, res) {
+		console.log("Function handleResult called: ", JSON.stringify(req.msg))
+	})
+
+	planner.executePlan(options.plan)
+
+}
+
+main()
--- planner/planner.js
+++ planner/planner.js
@@ -1,32 +1,21 @@
-const cmdArgs = require('command-line-args')
-const fs = require('fs')
 const YAML = require('yaml')
-const parserEventNames = require("./parsers/event_names").parser;
-const parserEventExpr = require("./parsers/event_expr").parser;
-const parserSymbols = require("./parsers/symbols").parser;
-const parserExpression = require("./parsers/expression").parser;
-const secureAmqp = require('../cllibsecureamqp')
+const fs = require('fs')
+const parserEventNames = require("../parsers/event_names").parser;
+const parserEventExpr = require("../parsers/event_expr").parser;
+const parserSymbols = require("../parsers/symbols").parser;
+const parserExpression = require("../parsers/expression").parser;
+const secureAmqp = require('../../cllibsecureamqp')
 //const secureAmqp = require('secureamqp')
 
-const cmdOptions = [
-	{ name: 'send', alias: 's', type: String},
-	{ name: 'config', alias: 'c', type: String},
-	{ name: 'plan', alias: 'p', type: String}
-]
-const options = cmdArgs(cmdOptions)
-options.config = options.config || "./config"
-const config = require(options.config)
-const toAddress = options.send
 const events = {}
-
-const _plan = (() => {
+let plan
+/*const _plan = (() => {
 	if(options.plan) {
 		const file = fs.readFileSync(options.plan, 'utf8')
 		const p = YAML.parse(file)
 		return p
 	} else return {}
-})()
-
+})()*/
 
 function replace(s, t) {
 	function tr(k) {
@@ -142,7 +131,6 @@ function parsePlan(p) {
 	}
 }
 
-let plan
 
 function addToTable(k, v, t) {
 	console.log("Adding to table: ", { key: k, value: v})
@@ -291,25 +279,14 @@ function debug() {
 	})
 }
 
-async function main() {
-	await secureAmqp.init(config)
-	const myAddress = secureAmqp.getMyAddress()
-	console.log("Actor address: ", myAddress)
-	plan = parsePlan(_plan)
-	console.log(plan.listeners)
-
-//	debug()
-
-	secureAmqp.registerFunction('.f.handleResult', null, function(req, res) {
-		console.log("Function handleResult called: ", JSON.stringify(req.msg))
-	})
-
+module.exports.executePlan = function(fileName) {
+	const fileContents = fs.readFileSync(fileName, 'utf8')
+	const p = YAML.parse(fileContents)
+	plan = parsePlan(p)
 	Object.keys(plan.events).forEach(k => {
-		console.log("Subscribing to event: ", k)
+		console.log("Lib Subscribing to event: ", k)
 		secureAmqp.subscribeEvent(k, function(e) {
 			newEvent(e)
 		})
 	})
 }
-
-main()
